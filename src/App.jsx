a80daec4d8d9c1691884925b03fbfe543231662e
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-04-05 06:40:46 -0500

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Work in progress</h1>
       <p>Rubik&apos;s cube game in development...</p>
     </div>
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.scss';
+import logo from '../../assets/static/images/logo.png';
+
+const Header = () => (
+  <header className="Header">
+    <img className="Header__logo" src={logo} alt="Logo" />
+    <h1 className="Header__title">
+      <span className="color--green">R</span>
+      <span className="color--grey">u</span>
+      <span className="color--blue">b</span>
+      <span className="color--orange">i</span>
+      <span className="color--yellow">k</span>
+      <span className="color--red">&apos;s</span>
+      {' '}
+      Cube Game
+    </h1>
+  </header>
+);
+
+export default Header;
