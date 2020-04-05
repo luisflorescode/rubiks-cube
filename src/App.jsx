06a9b165a3aab71eebc6a7284c19e2c79d26a0cb
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-04-05 07:20:27 -0500

    Add Cube component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cube from './components/Cube';
 import Header from './components/Header';
 import './App.scss';
 
@@ -6,6 +7,9 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className="App__content">
+        <Cube />
+      </div>
       <h1>Work in progress</h1>
       <p>Rubik&apos;s cube game in development...</p>
     </div>
---
+++ src/components/Cube/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Cube.scss';
+
+const Cube = () => (
+  <section className="Cube">
+    <img
+      src="http://cube.crider.co.uk/visualcube.php?fmt=svg&size=300&pzl=3&alg=S"
+      alt="Rubik's Cube"
+    />
+  </section>
+);
+
+export default Cube;
