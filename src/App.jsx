cac83ae1ba2439b086b5669c7578f8bacadc2871
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-04-05 03:01:44 -0500

    Initial environment created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Work in progress</h1>
+      <p>Rubik&apos;s cube game in development...</p>
+    </div>
+  );
+}
+
+export default App;
