4fc79030e5e6b948063b695aede02851cd439dac
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-04-06 01:47:58 -0500

    Add addMove action in cubeDuck

--- src/redux/cubeDuck.js
+++ src/redux/cubeDuck.js
@@ -7,15 +7,25 @@ const initialState = {
 };
 
 // Actions
-const SCRAMBLE = 'SCRAMBLE';
+const ADD_MOVE = 'ADD_MOVE';
 
 // Reducer
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case SCRAMBLE:
+    case ADD_MOVE:
+      return {
+        ...state,
+        moves: action.payload,
+      };
     default:
       return state;
   }
 }
 
 // Action Creators
+export const addMove = (move) => (dispatch) => {
+  dispatch({
+    type: ADD_MOVE,
+    payload: move,
+  });
+};
