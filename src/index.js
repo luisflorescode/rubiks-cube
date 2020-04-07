bb124911d6330370cc333f4ee35082d4204b8b84
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-04-07 17:43:14 -0500

    Add Redux state in local stoage to persist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -6,12 +7,23 @@ import generateStore from './redux/store';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 const store = generateStore();
 
 const WithStrictMode = () => <React.StrictMode><App /></React.StrictMode>;
 const WithStore = () => <Provider store={store}><WithStrictMode /></Provider>;
 
 ReactDOM.render(<WithStore />, document.getElementById('root'));
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,16 +4,31 @@ import {
 import thunk from 'redux-thunk';
 import cubeReducer from './cubeDuck';
 
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (error) {
+    return undefined;
+  }
+}
+
 const rootReducer = combineReducers({
   cube: cubeReducer,
 });
 
+const persistedState = loadFromLocalStorage();
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default function generateStore() {
+function generateStore() {
   const store = createStore(
     rootReducer,
+    persistedState,
     composeEnhancers(applyMiddleware(thunk)),
   );
   return store;
 }
+
+export default generateStore;
