f4b3730ea0007b43cc2d574975457eddbf8c90a1
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-04-08 03:43:59 -0500

    Add Link to / in Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.scss';
 import { ReactComponent as RubikSVG } from '../../assets/static/svg/rubik.svg';
 
 const Header = () => (
-  <header className="Header">
-    <RubikSVG className="Header__logo" />
-    <h1 className="Header__title">
-      <span className="color--green">R</span>
-      <span className="color--white">u</span>
-      <span className="color--blue">b</span>
-      <span className="color--orange">i</span>
-      <span className="color--yellow">k</span>
-      <span className="color--red">&apos;s</span>
-      {' '}
-      Cube Game
-    </h1>
+  <header>
+    <Link className="Header" to="/">
+      <RubikSVG className="Header__logo" />
+      <h1 className="Header__title">
+        <span className="color--green">R</span>
+        <span className="color--white">u</span>
+        <span className="color--blue">b</span>
+        <span className="color--orange">i</span>
+        <span className="color--yellow">k</span>
+        <span className="color--red">&apos;s</span>
+        {' '}
+        Cube Game
+      </h1>
+    </Link>
   </header>
 );
 
