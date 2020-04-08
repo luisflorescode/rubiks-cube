4c4faf8219daf1f987620ca3feee00edb2c5af38
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-04-08 00:52:18 -0500

    Update scripts

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders Work in progress', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Work in progress/i);
-  expect(linkElement).toBeInTheDocument();
-});
